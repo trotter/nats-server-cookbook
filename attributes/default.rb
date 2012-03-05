d97c21cd48e13a93aa20ffcfc73f15218453f24e
Trotter Cashion <cashion@gmail.com>
2012-03-05 13:33:36 -0800

    Use cloudfoundry_common pid and log dirs

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,10 @@
+include_attribute "cloudfoundry-common"
+
 default[:nats_server][:config_dir] = "/etc/nats"
 default[:nats_server][:host]       = "0.0.0.0"
 default[:nats_server][:port]       = "4222"
 default[:nats_server][:user]       = "nats"
 default[:nats_server][:password]   = "nats"
 
-default[:nats_server][:pid_file]   = "/var/run/nats-server.pid"
-default[:nats_server][:log_file]   = "/var/log/nats-server.log"
+default[:nats_server][:pid_file]   = File.join(node[:cloudfoundry_common][:pid_dir], "nats-server.pid")
+default[:nats_server][:log_file]   = File.join(node[:cloudfoundry_common][:log_dir], "nats-server.log")
