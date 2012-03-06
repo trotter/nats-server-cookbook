a2ccd277c18b2f59106978a59f2db3bd3377cb63
Trotter Cashion <cashion@gmail.com>
2012-03-05 19:38:34 -0800

    Include cloudfoundry-common default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+include_recipe "cloudfoundry-common"
 include_recipe "cloudfoundry-common::ruby_1_9_2"
 include_recipe "bluepill"
 
